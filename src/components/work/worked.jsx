eeb33143d651872f7dd28bbb5b0dd09a2fbfcf5c
Son L <ahmadlekhareza@gmail.com>
2024-05-13 10:52:23 +0700

    menghapus text, dan mengubah judul

--- src/components/work/worked.jsx
+++ src/components/work/worked.jsx
@@ -11,24 +11,16 @@ const worked = () => {
           <span>shoe</span>
           <span>shoe</span>
         </div>
-        <span className='workOpening'>Featured projects</span>
+        <span className='workOpening'>Store</span>
       </div>
       <div className="workContainerMain">
       <div className="workMain">
         <img className='workImage' src={Picture} alt="" />
-        <div className="workText">
-            <span className="workNumber">01</span>
-            <div className="workTitle">
-              <span>Panggung Gembira</span>
-              <span>Evolutioners Zealous</span>
-            </div>
-        </div>
       </div>
       <div className="workLink">
         <a className='workLinked' href="https://maps.app.goo.gl/iwnybzvaLurrRKby5?g_st=iw" target="_blank">
         <i class="uil uil-arrow-up-left"></i>
         <span>Norrmalm, Stockholm, Swedia</span>
-
         </a>
       </div>
       </div>
